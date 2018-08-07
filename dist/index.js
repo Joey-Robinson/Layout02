2eb54e8fe3afbbf4f3c9969c01027a8d656e02c1
elamahpla <theamazingness@gmail.com>
2018-08-07 15:15:27 -0500

    Adding a visibility toggle to js file, will use later.

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
+// Modal
 const openModal = () => {
   modal.style.display = 'block';
 }
@@ -18,3 +19,12 @@ const closeBtn = document.getElementsByClassName('home-one__button__close')[0];
 modalBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
 window.addEventListener('click', outsideClick);
+
+// Visibility Toggle
+// const open = () => {
+//   const hidden = document.getElementById('hidden');
+//   hidden.style.display = hidden.style.display == 'none' ? 'inline-block' : 'none';
+// }
+
+// const visibility = document.getElementById('clicky');
+// visibility.addEventListener('click', open)
