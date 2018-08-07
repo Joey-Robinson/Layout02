05b21573998e4c09737d3d6e4f5188cd3ff93759
elamahpla <theamazingness@gmail.com>
2018-08-07 12:12:18 -0500

    Changing JS to ES6.

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,16 @@
+const openModal = () => {
+  modal.style.display = 'block';
+}
+
+const closeModal = () => {
+  modal.style.display = 'none';
+}
+
+const outsideClick = (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+}
 const modal = document.getElementById('home-one__modal');
 const modalBtn = document.getElementById('home-one__button');
 const closeBtn = document.getElementsByClassName('home-one__button__close')[0];
@@ -5,17 +18,3 @@ const closeBtn = document.getElementsByClassName('home-one__button__close')[0];
 modalBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
 window.addEventListener('click', outsideClick);
-
-function openModal() {
-  modal.style.display = 'block';
-}
-
-function closeModal() {
-  modal.style.display = 'none';
-}
-
-function outsideClick(e) {
-  if (e.target === modal) {
-    modal.style.display = 'none';
-  }
-}
